f1c5ec5bc14cbd8d6dfe4c3f8d781777e7990573
Neha Singh <neha.singh931126@gmail.com>
2023-03-18 19:26:25 +0530

    Added boostrap card css classes and listing of post rendered using the map function of Javascript

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import PostCreate from './PostCreate';
+import PostList from "./PostList";
 const App = () => {
-  return <div className="container"><h1>Blog app</h1><PostCreate/></div>;
-
+  return <div className="container"><h1>Blog app</h1>
+    <PostCreate/>
+    
+    <PostList/>
+  </div>;
 };
 
 export default App;
---
+++ client/src/PostList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const PostList = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await axios.get('http://localhost:4000/posts');
+            setPosts(res.data);
+        }; fetchPosts()
+    }, []);
+
+    const postListJSX =
+        posts.map((post) => {
+            return (
+                <div className="card" key={post.id}
+                    style={{ width: '30%', marginBottom: '20px' }}>
+                    <div className="card-body">
+                        <h5 className="card-title">{post.title}</h5>
+                    </div>
+                </div>
+            )
+        })
+    return (
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            {postListJSX}
+        </div>
+    )
+};
+
+export default PostList;
